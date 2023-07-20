da333611ba73c72a8c324a558abd0f5bd3e07180
kyl3x <130746066+kyl3x@users.noreply.github.com>
2023-07-20 10:46:02 +0100

    added procs

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn indiseov2.wsgi:application --log-file -
--- ingseo/settings.py
+++ ingseo/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +145,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # EMAIL_HOST_USER = 'hi@email.com'
 # EMAIL_HOST_PASSWORD = 'password'
 # DEFAULT_FROM_EMAIL = 'hi@email.com'
+
+django_heroku.settings(locals())
